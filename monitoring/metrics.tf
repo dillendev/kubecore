ddb0128e38f8634786473bc540d4fdee5990a661
Dillen Meijboom <work@dillen.dev>
2023-03-29 09:39:08 +0200

    feat: add the option to disable metrics-server in the monitoring module

--- monitoring/metrics.tf
+++ monitoring/metrics.tf
@@ -1,4 +1,5 @@
 resource "helm_release" "metrics-server" {
+  count            = var.metrics.enabled ? 1 : 0
   name             = "metrics-server"
   repository       = "https://kubernetes-sigs.github.io/metrics-server/"
   chart            = "metrics-server"
--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -1,5 +1,9 @@
 variable "metrics" {
   type = object({
     chart_version = string
+    enabled       = bool
   })
+  default = {
+    enabled = false
+  }
 }
